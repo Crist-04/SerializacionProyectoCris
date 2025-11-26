a784eaa00a3107949db60abd5bcb54bf611eb123
digis <digis@192.167.1.7>
2025-11-26 16:19:47 -0600

    Se modifico el CorsFilterConfiguration para que se pudiera actualizar editar y eliminar direccion

--- src/main/java/com/digis01/CAlvarezProgramacionNCapasOctubre2025/Configuration/CorsFilterConfiguration.java
+++ src/main/java/com/digis01/CAlvarezProgramacionNCapasOctubre2025/Configuration/CorsFilterConfiguration.java
@@ -2,31 +2,32 @@ package com.digis01.CAlvarezProgramacionNCapasOctubre2025.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsFilterConfiguration {
-
+    
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfig = new CorsConfiguration();
-
-        corsConfig.addAllowedOriginPattern("http://localhost:8080");
-
-        corsConfig.addAllowedMethod(HttpMethod.GET);
-        corsConfig.addAllowedMethod(HttpMethod.POST);
-        corsConfig.addAllowedMethod(HttpMethod.PUT);
-        corsConfig.addAllowedMethod(HttpMethod.PATCH);
-        corsConfig.addAllowedMethod(HttpMethod.DELETE);
-
+        
+        corsConfig.addAllowedOriginPattern("http://localhost:*");
+        
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("PUT");
+        corsConfig.addAllowedMethod("PATCH");
+        corsConfig.addAllowedMethod("DELETE");
+        corsConfig.addAllowedMethod("OPTIONS"); 
+        
         corsConfig.addAllowedHeader("*");
-
+        
+        corsConfig.setAllowCredentials(true);
+        
         source.registerCorsConfiguration("/**", corsConfig);
         return new CorsFilter(source);
     }
-
 }
